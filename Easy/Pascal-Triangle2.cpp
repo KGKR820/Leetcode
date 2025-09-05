ebe55e0fb32039928501ed6c9864ed9c6969b702
KGKR <kgkr6018@gmail.com>
2025-09-05 21:51:19 +0530

    Regular

--- Easy/Pascal-Triangle2.cpp
+++ Easy/Pascal-Triangle2.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+    vector<int> getRow(int rowIndex){
+      vector<int> output;
+      long val =1;
+      for(int i=0;i<rowIndex;i++){
+        output.push_back((int) val);
+        val = val*(rowIndex-i)/(i+1);
+      }
+      return output;
+    }
 int main(){
-
+    
     return 0;
 }
